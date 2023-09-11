2aac7bb4611f370bdd84ed37082f3bd28daea6be
Barel Shraga <barel123133@gmail.com>
2023-09-11 18:39:50 +0300

    change inner key name in the Props type

--- src/components/Weather/WeatherForecast.tsx
+++ src/components/Weather/WeatherForecast.tsx
@@ -1,6 +1,6 @@
 type Props = {
   forecast: {
-    [cityName: string]: {
+    [day: string]: {
       temperatureRange: string;
     };
   };
@@ -10,15 +10,15 @@ function WeatherForecast({ forecast }: Props) {
   return (
     <div className="flex flex-row overflow-auto md:justify-center">
       {forecast &&
-        Object.keys(forecast).map((cityName, i) => (
+        Object.keys(forecast).map((day, i) => (
           <div
             key={i}
             className={`py-6 px-3 border-r border-t border-b ${
               !i ? 'border-l' : ''
             } border-slate-900 min-w-[140px]`}>
-            <h2 className="text-xl leading-loose">{cityName}</h2>
+            <h2 className="text-xl leading-loose">{day}</h2>
             <h2 className="max-md:text-sm">
-              {forecast[cityName].temperatureRange}
+              {forecast[day].temperatureRange}
             </h2>
           </div>
         ))}
